ad8a03e9392967be2f5250ac7c60abb3cdbb04a2
Dan Moffat <danmoffat@gmail.com>
2024-04-26 22:26:00 +0100

    Added nav styling plus active link styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,30 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
-    <nav>
-      <Link href='/'>Home</Link>
-      <Link href='/performance'>Performance</Link>
-      <Link href='/reliability'>Reliability</Link>
-      <Link href='/scale'>Scale</Link>
+    <nav className='w-full absolute z-10 flex flex-wrap terms-center justify-between mx-auto p-8'>
+      <Link className={pathname === '/' ? 'font-bold' : ''} href='/'>
+        Home
+      </Link>
+      <Link
+        className={pathname === '/performance' ? 'font-bold' : ''}
+        href='/performance'>
+        Performance
+      </Link>
+      <Link
+        className={pathname === '/reliability' ? 'font-bold' : ''}
+        href='/reliability'>
+        Reliability
+      </Link>
+      <Link className={pathname === '/scale' ? 'font-bold' : ''} href='/scale'>
+        Scale
+      </Link>
     </nav>
   );
 }
