d2df755e15ebef75076f37192acd157ae73e9cd2
Dan Moffat <danmoffat@gmail.com>
2024-04-26 14:46:13 +0100

    A little styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main>
-      <Image src='/images/home.jpg' alt='Factory' fill />
+    <main className='absolute -z-10 inset-0'>
+      <Image
+        className='object-cover'
+        src='/images/home.jpg'
+        alt='Factory'
+        fill
+      />
     </main>
   );
 }
