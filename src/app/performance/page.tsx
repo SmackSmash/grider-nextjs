2571140410d9a1bf8dc120f0247bd6da32b23e0f
Dan Moffat <danmoffat@gmail.com>
2024-04-26 21:34:09 +0100

    Removed naughty language

--- src/app/performance/page.tsx
+++ src/app/performance/page.tsx
@@ -5,7 +5,7 @@ export default function Performance() {
   return (
     <main>
       <Hero imgSrc={PerformanceImg} imgAlt='Welding'>
-        <h1 className='text-4xl font-bold'>Look at this welding shit!</h1>
+        <h1 className='text-4xl font-bold'>Look at this welding!</h1>
       </Hero>
     </main>
   );
