5a9c9197e562fe2032c406c2c2f6d3243cdf62ac
Dan Moffat <danmoffat@gmail.com>
2024-04-26 14:37:12 +0100

    Added image to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <main>Hello there</main>;
+  return (
+    <main>
+      <Image src='/images/home.jpg' alt='Factory' fill />
+    </main>
+  );
 }
